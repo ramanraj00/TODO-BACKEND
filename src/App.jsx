5da379cceb683950b1a44fc2ee0e35077c06686b
ramanraj00 <r02519625@gmail.com>
2026-01-01 18:24:52 +0530

    Todo Frontend page done

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,52 @@ import './App.css'
 function App() {
 
   const [todo,setTodo] = useState([]);
-
-  function deletetodo () {
-
-  }
+  const [input,setInput] = useState("");
 
   function addtodo () {
-    
+    if (input.trim()==="") return;
+
+    setTodo([
+        ...todo, {
+            id:Date.now(),
+            text:input,
+            completed:false
+        }
+    ]);
+
+    setInput("");
+
   }
 
+
+  function deletetodo (id) {
+    setTodo(todo.filter((item)=> item.id !== id))
+  }
+
+
   return <div>
-    <button onClick={deletetodo}>delete todo</button>
+    <input 
+           type="text"
+           value={input}
+           onChange={(e)=>setInput(e.target.value)}
+           placeholder='Enter Todo'
+    />
+
     <button onClick={addtodo}>Add todo</button>
+
+    <ul>
+        {todo.map((item)=> (
+            <li key={item.id}>
+                    {item.text}
+                <button onClick={()=>deletetodo(item.id)}>Delete Todo</button>
+            </li>
+        ))}
+    </ul>
   </div>
 
 }
 
 export default App
+
+
+
